f5cf5be08bf541946dd1e94a437cf1cdd9c69fb8
김진우 <kjw2122@naver.com>
2020-05-27 23:09:51 +0900

    Spring Data Common: Null 처리

--- src/test/java/me/daroguzo/demospringdata/CommentRepositoryTest.java
+++ src/test/java/me/daroguzo/demospringdata/CommentRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,14 +21,7 @@ public class CommentRepositoryTest {
 
     @Test
     public void crud(){
-        Comment comment = new Comment();
-        comment.setComment("Hello Comment");
-        commentRepository.save(comment);
 
-        List<Comment> all = commentRepository.findAll();
-        assertThat(all.size()).isEqualTo(1);
-
-        long count = commentRepository.count();
-        assertThat(count).isEqualTo(1);
+        commentRepository.save(null);
     }
 }
